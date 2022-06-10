2db9cf1c4d9fac91c43731805413b5bf41e07464
Demi4nT <demhernandezmont21@gmail.com>
2022-06-10 01:12:28 +0000

    tarea terminada

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,17 +1,27 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
 const Home = () => {
+	const [color, setColor] = useState("red");
 	return (
 		<div id="traffic-light">
 			<div className="traffic-top"></div>
 			<div className="container">
-				<div className="light red"></div>
-				<div className="light yellow"></div>
-				<div className="light green"></div>
+				<div
+					onClick={() => setColor("red")}
+					className={
+						"light red" + (color === "red" ? " glow " : "")
+					}></div>
+				<div
+					onClick={() => setColor("yellow")}
+					className={
+						"light yellow" + (color === "yellow" ? " glow " : "")
+					}></div>
+				<div
+					onClick={() => setColor("green")}
+					className={
+						"light green" + (color === "green" ? " glow " : "")
+					}></div>
 			</div>
 		</div>
 	);
